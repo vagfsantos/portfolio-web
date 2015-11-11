f52cc417df23d542dea9bc266a3a1a17363ad7d8
Vagner Santos <santos.vagnerferreira@gmail.com>
2015-11-11 01:32:07 -0200

    Corrindo interface mobile e adicionando fotos dos projetos

--- js/script.js
+++ js/script.js
@@ -2,5 +2,16 @@ window.addEventListener("load", carregou);
 
 function carregou(){
 	document.querySelector("#carregando").style.display = "none";
-	new WOW().init();
+	var botaoMenu = document.querySelector("#btn-menu");
+
+	function menuMobile(){
+		
+		$(botaoMenu).find('span').classList.toggle("glyphicon-remove");
+	}
+
+	botaoMenu.addEventListener("click", menuMobile);
+
+	if(window.innerWidth > 800){
+		new WOW().init();
+	}
 }
